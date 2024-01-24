abe9c376eee4f86e0487c5c93573e01f9ecf3814
erichestella <erichestella05@gmail.com>
2024-01-24 13:45:29 +0800

    to string the numebr you input or type

--- print reversed numbers.py
+++ print reversed numbers.py
@@ -3,3 +3,6 @@ print('\nEXTRACTING THE REVERSED NUMBERS.')
 
 #for the numbers you want to type   
 numeral = int(input('\nTYPE ANY NUMBERS: '))
+
+#this is for to string the numbers you input 
+define_numerical = str(numeral)

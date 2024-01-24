b8f0655ff2dcaa7ba4dce63f8d4bc703eb626d89
erichestella <erichestella05@gmail.com>
2024-01-24 13:55:09 +0800

    it reversed the numbers you input

--- print reversed numbers.py
+++ print reversed numbers.py
@@ -6,3 +6,7 @@ numeral = int(input('\nTYPE ANY NUMBERS: '))
 
 #this is for to string the numbers you input 
 define_numerical = str(numeral)
+
+#it reversed the numbers you input
+checking_digital = define_numerical([:: -1])
+print('\nREVERSED NUMBERS :'," ".join(checking_digital), '\n')
